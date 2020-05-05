dbf2151acf58381517bb6681605112abc90e00be
Andrey Prisniak <aprisniak@lectrum.io>
2020-05-05 20:30:00 +0300

    1

--- src/js/module.js
+++ src/js/module.js
@@ -1,3 +1,13 @@
 {
-    console.log('Hello Lectrum!');
+    const form = document.querySelector('.createTaskForm');
+
+    // form.onsubmit = function() {}
+    form.onsubmit = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const task = formData.get('task');
+
+        console.log(task);
+    }
 }
