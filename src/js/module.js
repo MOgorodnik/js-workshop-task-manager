8f69552fcf2609073bacaf2f9d6310dbc818ba2c
Andrey Prisniak <aprisniak@lectrum.io>
2020-05-05 20:45:42 +0300

    2

--- src/js/module.js
+++ src/js/module.js
@@ -9,5 +9,30 @@
         const task = formData.get('task');
 
         console.log(task);
+        addTask(task);
+
+        form.reset();
+    }
+
+    const getUpdatedTemplate = (task) => `
+        <div class="content">
+            <button class="checkbox"></button>
+            <input type="text" disabled value="${task}" class="taskInput"/>
+        </div>
+        <div class="actions">
+            <button class="star"></button>
+            <button class="edit"></button>
+            <button class="remove"></button>
+        </div>
+    `;
+
+    const addTask = (task) => {
+        // взять ul элемент
+        const tasksDom = document.querySelector('.tasks');
+        // создать li элемент
+        const taskDom = document.createElement('li');
+        taskDom.innerHTML = getUpdatedTemplate(task);
+        
+        tasksDom.prepend(taskDom);
     }
 }
